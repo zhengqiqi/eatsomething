f89c04217b4cee4e1a393eb790e18811daa5eebf
zhengqiqi <zhengqiqi_7@163.com>
2015-07-11 23:01:04 +0800

    首页专题完善,小点居中,详情页完善

---
+++ script/index.js
@@ -0,0 +1,5 @@
+$('#content .dish-1, #content .dish-2, #content .dish-3, #content .subject_con_sub').mouseover(function(){
+    $(this).addClass('back-change');
+}).mouseleave(function(){
+    $(this).removeClass('back-change');
+})
