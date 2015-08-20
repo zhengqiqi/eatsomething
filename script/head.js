4e2472473b28e516edd3ee0a0d49412291b18757
zhengqiqi <zhengqiqi_7@163.com>
2015-08-20 23:16:15 +0800

    页面头部

--- script/head.js
+++ script/head.js
@@ -18,22 +18,22 @@ var headHtml =
         '</div>'+
     '</div>'+
     '<div class="headline">'+
-        '<div class="headline_big"><a href="#" class="headline_big_a">做吃的</a></div>'+
-        '<div class="headline_small"><p>下厨不是件容易的事，每道菜都有它的脾气。</p></div>'+
+        '<div class="headline_big"><a href="./index.html" class="headline_big_a">做吃的</a></div>'+
+        '<div class="headline_small">下厨不是件容易的事，每道菜都有它的脾气。</div>'+
     '</div>'+
     '<div class="control">'+
-        '<span class="search">'+
+        '<div class="search">'+
             '<form action="#" method="post">'+
-                '<input type="text" name="search" class="text" value="糖醋排骨"/>'+
-                '<input type="image" src="images/index_submit.png" class="submit search-btn"/>'+
+                '<input type="text" name="search" class="search-text" value="糖醋排骨"/>'+
+                '<input type="submit" class="submit search-btn" value="搜索"/>'+
             '</form>'+
-        '</span>'+
+        '</div>'+
     '</div>'+
 '</div>';
 
 $('html body').prepend(headHtml);
 
-$('#header input.text').focus(function(){
+$('#header .search-text').focus(function(){
     if ($(this).val() == "糖醋排骨"){
         $(this).addClass('text_focus').val('');
     }
