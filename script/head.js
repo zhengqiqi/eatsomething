29a8491333c935f668792874a0f719cf35d293bb
zhengqiqi <zhengqiqi_7@163.com>
2015-08-27 23:14:50 +0800

    頭部

--- script/head.js
+++ script/head.js
@@ -1,5 +1,51 @@
 var headHtml = 
 '<div id="header">'+
+    '<div id="head-top-2">'+
+        '<a href="./index.html"><div class="head-top-title-2 title-color">做吃的</div></a>'+
+        '<div class="head-top-title2-2 title-color">下厨不是件容易的事，每道菜都有它的脾气。</div>'+
+        '<div class="head-top-search-2">'+
+            '<form action="#" method="post">'+
+                '<input type="text" name="search" class="head-top-search-text-2" value="糖醋排骨"/>'+
+                '<input type="submit" class="submit head-top-search-btn-2" value="搜索"/>'+
+            '</form>'+
+        '</div>'+
+        '<div class="head-top-link-2 nologin hide">'+
+                '<div class="head-top-link-each-2">'+
+                    '<a href="./index.html"><div class="head-top-link-each-p-2 title-color">首页</div></a>'+
+                '</div>'+
+                '<div class="head-top-link-each-2">'+
+                    '<a href="javascript:;"><div class="head-top-link-each-p-2 title-color">分类大全</div></a>'+
+                '</div>'+
+                '<div class="head-top-link-each-break-2">|</div>'+
+                '<div class="head-top-link-each-2">'+
+                    '<a href="javascript:;"><div class="head-top-link-each-p-2 title-color">注册</div></a>'+
+                '</div>'+
+                '<div class="head-top-link-each-2">'+
+                    '<a href="javascript:;"><div class="head-top-link-each-p-2 title-color">登陆</div></a>'+
+                '</div>'+
+        '</div>'+
+        '<div class="head-top-link-2 logined">'+
+                '<div class="head-top-link-each-2">'+
+                    '<a href="./index.html"><div class="head-top-link-each-p-2 title-color">首页</div></a>'+
+                '</div>'+
+                '<div class="head-top-link-each-2">'+
+                    '<a href="javascript:;"><div class="head-top-link-each-p-2 title-color">分类大全</div></a>'+
+                '</div>'+
+                '<div class="head-top-link-each-break-2">|</div>'+
+                '<div class="head-top-link-each-2">'+
+                    '<a href="./cooker-published.html"><div class="head-top-link-each-p-2 title-color">用户名</div></a>'+
+                '</div>'+
+                '<div class="head-top-link-each-2">'+
+                    '<a href="javascript:;"><div class="head-top-link-each-p-2 title-color">我要发布</div></a>'+
+                '</div>'+
+                '<div class="head-top-link-each-2">'+
+                    '<a href="./published.html"><div class="head-top-link-each-p-2 title-color">关于我的</div></a>'+
+                '</div>'+
+                '<div class="head-top-link-each-2">'+
+                    '<a href="javascript:;"><div class="head-top-link-each-p-2 title-color">退出</div></a>'+
+                '</div>'+
+            '</div>'+
+    '</div>';
 /*
     '<div id="header-top">'+
         '<div class="header-top-div nologin hide">'+
@@ -36,7 +82,7 @@ var headHtml =
 */
 $('html body').prepend(headHtml);
 
-$('#header .search-text').focus(function(){
+$('#header .head-top-search-text-2').focus(function(){
     if ($(this).val() == "糖醋排骨"){
         $(this).addClass('text_focus').val('');
     }
@@ -47,6 +93,7 @@ $('#header .search-text').focus(function(){
     }
 })
 
+/*
 $('#login_h').mouseover(function(){
     $('#login_hide').removeClass('hide');
 }).mouseleave(function(){
@@ -66,3 +113,4 @@ $('#header-top .header-top-name').mouseover(function(){
 $('#header-top .logined').mouseleave(function(){
     $(this).children('.header-top-name-hide').addClass('hide')
 })
+*/
--- script/index.js
+++ script/index.js
@@ -3,3 +3,15 @@ $('#content .dish-1, #content .dish-2, #content .dish-3, #content .subject_con_s
 }).mouseleave(function(){
     $(this).removeClass('back-change-index');
 })
+
+$(function(){
+    var topHeight = $('.head-top-2').offsetTop();      //获取头部对于页面顶部的距离
+    $(window).scroll(function(){
+        var topScro = $(this).scrollTop();      //获取滚动条距顶部的距离
+        if(topScro >= topHeight){
+            $('.head-top-2').css({"position":"fixed","top":0});
+        }else if(topScro < topHeight){
+            $('.head-top-2').css({"position":"static"});
+        }
+    })
+})
