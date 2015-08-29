f89cef54781f8a3ffaf9c9ba0c3e20e8a380e7ec
zhengqiqi <zhengqiqi_7@163.com>
2015-08-29 23:23:59 +0800

    首页改版

--- script/index.js
+++ script/index.js
@@ -3,3 +3,25 @@ $('#content .dish-1, #content .dish-2, #content .dish-3, #content .subject_con_s
 }).mouseleave(function(){
     $(this).removeClass('back-change-index');
 })
+
+$('#index-right-nologin .input-address').focus(function(){
+    if ($(this).val() == "请输入您的邮箱地址"){
+        $(this).addClass('text_focus').val('');
+    }
+}).blur(function(){
+    var $this = $(this);
+    if ($this.val() ==""){
+        $this.val('请输入您的邮箱地址').removeClass('text_focus');
+    }
+})
+
+$('#index-right-nologin .input-password').focus(function(){
+    if ($(this).val() == "请输入您的密码"){
+        $(this).addClass('text_focus').val('');
+    }
+}).blur(function(){
+    var $this = $(this);
+    if ($this.val() ==""){
+        $this.val('请输入您的密码').removeClass('text_focus');
+    }
+})
