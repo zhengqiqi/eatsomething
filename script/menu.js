c71f028e75d1540e796163221b88f8cbd91ba5c2
zhengqiqi <zhengqiqi_7@163.com>
2015-09-10 23:34:11 +0800

    弹出框

--- script/menu.js
+++ script/menu.js
@@ -1,1 +1,7 @@
-$(".modern-menu").modernMenu();
+/*$('#menu').click(function(){
+    window.open('menu-open.html',"height=500, width=700, toolbar=no,menubar=no, scrollbars=no, resizable=no, location=no, status=no")
+})*/
+
+$('#menu .menu-each-right-button').click(function(){
+    $(this).parents('#menu').siblings('#menu-open').removeClass('hide');
+})
