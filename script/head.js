db2a7775a8c37a7cc0ef4e61c09697806c5050db
zhengqiqi <zhengqiqi_7@163.com>
2015-09-22 00:04:57 +0800

    发布-菜单

--- script/head.js
+++ script/head.js
@@ -63,40 +63,7 @@ var footerHtml =
             '<div class="footer-secend-each"><a href="http://www.douguo.com/">豆果美食</a></div>'+
         '</div>'+
     '</div>';
-/*
-    '<div id="header-top">'+
-        '<div class="header-top-div nologin hide">'+
-            '<a href="javascript:;"><div class="header-top-btn header-top-btn-left">注册</div></a>'+
-            '<a href="javascript:;"><div class="header-top-btn header-top-btn-right">登陆</div></a>'+
-        '</div>'+
-        '<div class="header-top-div logined">'+
-            '<div><a href="javascript:;" class="header-top-btn header-top-btn-left header-top-name">用户名</a></div>'+
-            '<div><a href="javascript:;" class="header-top-btn header-top-btn-right">退出</a></div>'+
-            '<div class="header-top-name-hide hide">'+
-                '<ul>'+
-                    '<li><a href="javascript:;" class="header-top-hide-each">我要发布</a></li>'+
-                    '<li><a href="./published.html" class="header-top-hide-each">我的信息</a></li>'+
-                    '<li><a href="./set.html" class="header-top-hide-each">个人设置</a></li>'+
-                '</ul>'+
-            '</div>'+
-        '</div>'+
-    '</div>'+
-    '<div id="header-mid">'+
-        '<div class="headline">'+
-            '<div class="headline_big"><a href="./index.html" class="headline_big_a">做吃的</a></div>'+
-            '<div class="headline_small">下厨不是件容易的事，每道菜都有它的脾气。</div>'+
-        '</div>'+
-        '<div class="control">'+
-            '<div class="search">'+
-                '<form action="#" method="post">'+
-                    '<input type="text" name="search" class="search-text" value="糖醋排骨"/>'+
-                    '<input type="submit" class="submit search-btn" value="搜索"/>'+
-                '</form>'+
-            '</div>'+
-        '</div>'+
-    '</div>'+
-'</div>';
-*/
+    
 $('html body').prepend(headHtml);
 
 $('html body').append(footerHtml);
@@ -111,28 +78,6 @@ $('#header .head-top-search-text-2').focus(function(){
         $this.val('糖醋排骨').removeClass('text_focus');
     }
 })
-
-/*
-$('#login_h').mouseover(function(){
-    $('#login_hide').removeClass('hide');
-}).mouseleave(function(){
-    $('#login_hide').addClass('hide');
-})
-
-$('#header-top .header-top-btn').mouseover(function(){
-    $(this).addClass('header-top-btn-hover')
-}).mouseleave(function(){
-    $(this).removeClass('header-top-btn-hover')
-})
-
-$('#header-top .header-top-name').mouseover(function(){
-    $(this).parent().siblings('.header-top-name-hide').removeClass('hide');
-})
-
-$('#header-top .logined').mouseleave(function(){
-    $(this).children('.header-top-name-hide').addClass('hide')
-})
-*/
 
 $(function(){
     var topHeight = $('#head-top-2').offset().top;      //获取头部对于页面顶部的距离
--- script/publish.js
+++ script/publish.js
@@ -1,39 +1,21 @@
-$('#publish .publish-input-stuff-name').focus(function(){
-    var $this = $(this)
-    if ($this.val() == "食材"){
-        $this.removeClass('text-unfocus').val('');
-    }
-}).blur(function(){
-    var $this = $(this);
-    if ($this.val() ==""){
-        $this.val('食材').addClass('text-unfocus');
-    }
-})
-
-$('#publish .publish-input-stuff-num').focus(function(){
-    var $this = $(this)
-    if ($this.val() == "数量"){
-        $this.removeClass('text-unfocus').val('');
-    }
-}).blur(function(){
-    var $this = $(this);
-    if ($this.val() ==""){
-        $this.val('数量').addClass('text-unfocus');
-    }
-})
-
-var publishInputAdd=
+var publishInputAdd = 
 '<div class="publish-input-div-add">'+
-    '<input class="publish-input-stuff-name text-unfocus" type="text" name="stuff" value="食材" />'+
-    '<input class="publish-input-stuff-num text-unfocus" type="text" name="number" value="数量" />'+
+    '<input class="publish-input-stuff-name" type="text" name="stuff" placeholder="食材" />'+
+    '<input class="publish-input-stuff-num" type="text" name="number" placeholder="数量" />'+
     '<input class="publish-input-stuff-btn-add" type="button" name="button-add" />'+
     '<input class="publish-input-stuff-btn-sub" type="button" name="button-sub" />'+
 '</div>';
 
-$('#publish .publish-input-stuff-btn-add').click(function(){
+/*$('#publish .publish-input-stuff-btn-add').click(function(){
     var $this = $(this)
-    $this.parent().parent().append(publishInputAdd);
+    $this.parent().after(publishInputAdd);
+})*/
+
+$('#publish .publish-input-content').delegate(".publish-input-stuff-btn-add","click",function(){
+    var $this = $(this)
+    $this.parent().after(publishInputAdd);
 })
+
 
 $('#publish .publish-input-stuff-btn-sub').click(function(){
     var $this = $(this)
