9e989f3f8affb06bb7d5640a2530457ae852c60d
zhengqiqi <zhengqiqi_7@163.com>
2015-09-24 23:09:12 +0800

    发布页面

--- script/publish.js
+++ script/publish.js
@@ -1,4 +1,4 @@
-var publishInputAdd = 
+/*var publishInputAdd = 
 '<div class="publish-input-div-add">'+
     '<input class="publish-input-stuff-name" type="text" name="stuff" placeholder="食材" />'+
     '<input class="publish-input-stuff-num" type="text" name="number" placeholder="数量" />'+
@@ -13,5 +13,25 @@ $('#publish .publish-input-content').delegate(".publish-input-stuff-btn-add","cl
 
 $('#publish .publish-input-content').delegate(".publish-input-stuff-btn-sub","click",function(){
     var $this = $(this)
-    $this.parent().remove();
+    $this.parent().remove();children().append(publishInputAdd2)
+})*/
+
+var publishInputAdd2 = 
+'<tr>'+
+    '<td class="food">'+
+        '<input type="text" name="food" placeholder="食材"/>'+
+    '</td>'+
+    '<td class="num">'+
+        '<input type="text" name="num" placeholder="数量"/>'+
+    '</td>'+
+'</tr>';
+
+/*$('#publish .publish-content').delegate("button", "click", function(){
+    var $this = $(this)
+    $this.siblings().addClass('test');
+})*/
+
+$("#publish .publish-content button").click(function(){
+    var $this = $(this)
+    $this.siblings().addClass('test')
 })
